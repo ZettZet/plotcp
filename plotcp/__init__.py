715483b494e0c569072dda393c5210b0e2c5da34
Winkel <dmesser@yandex.ru>
2020-05-25 23:05:08 +0500

    add inits_only parametr

--- plotcp/__init__.py
+++ plotcp/__init__.py
@@ -21,7 +21,10 @@ def plotcp(
     inits_point=None,
     faxis=BOTH,
     reim=BOTH,
+    inits_only=False,
 ):
+    if inits_only == True and inits_point == None:
+        raise ValueError("'inits_point' is None")
 
     if faxis not in [ORIG, TRANSF, BOTH]:
         raise ValueError("'faxis' must be 'origin', 'trasform' or 'both'")
@@ -41,81 +44,89 @@ def plotcp(
     v_re = []
     v_im = []
     
-    if faxis == TRANSF or faxis == BOTH:
-        x = np.linspace(xbound[0], xbound[1], 2*gridstep)
-        y = np.linspace(ybound[0], ybound[1], 2*gridstep)
+    if inits_only == False:
+        if faxis == TRANSF or faxis == BOTH:
+            x = np.linspace(xbound[0], xbound[1], 2*gridstep)
+            y = np.linspace(ybound[0], ybound[1], 2*gridstep)
 
-        if reim == RE or reim == BOTH:
-            x_parallels = [[item_x + item_y*I for item_x in np.linspace(xbound[0], xbound[1], nsteps)] for item_y in y]
-            u = [[fun(item) for item in xs] for xs in x_parallels]
-            u_re = [np.array([re(item) for item in us]) for us in u]
-            u_im = [np.array([im(item) for item in us]) for us in u]
+            if reim == RE or reim == BOTH:
+                x_parallels = [[item_x + item_y*I for item_x in np.linspace(xbound[0], xbound[1], nsteps)] for item_y in y]
+                u = [[fun(item) for item in xs] for xs in x_parallels]
+                u_re = [np.array([re(item) for item in us]) for us in u]
+                u_im = [np.array([im(item) for item in us]) for us in u]
 
-        if reim == IM or reim == BOTH:
-            y_parallels = [[item_x + item_y*I for item_y in np.linspace(ybound[0], ybound[1], nsteps)] for item_x in x]
-            v = [[fun(item) for item in ys] for ys in y_parallels]
-            v_re = [np.array([re(item) for item in vs]) for vs in v]
-            v_im = [np.array([im(item) for item in vs]) for vs in v]
+            if reim == IM or reim == BOTH:
+                y_parallels = [[item_x + item_y*I for item_y in np.linspace(ybound[0], ybound[1], nsteps)] for item_x in x]
+                v = [[fun(item) for item in ys] for ys in y_parallels]
+                v_re = [np.array([re(item) for item in vs]) for vs in v]
+                v_im = [np.array([im(item) for item in vs]) for vs in v]
 
-    if reim == RE:
-        if faxis == ORIG:
-            ax.yaxis.set_major_locator(ticker.MultipleLocator(y_step))
-            ax.grid(which='major', axis='y', color='b')
+        if reim == RE:
+            if faxis == ORIG:
+                ax.yaxis.set_major_locator(ticker.MultipleLocator(y_step))
+                ax.grid(which='major', axis='y', color='b')
 
-        elif faxis == TRANSF:
-            for r, i in zip(u_re, u_im):
-                ax.plot(r, i, color='b')
+            elif faxis == TRANSF:
+                for r, i in zip(u_re, u_im):
+                    ax.plot(r, i, color='b')
 
-        else:
-            ax.yaxis.set_major_locator(ticker.MultipleLocator(y_step))
-            ax.grid(which='major', axis='y', color='b')
-            for r, i in zip(u_re, u_im):
-                ax.plot(r, i, color='b')
+            else:
+                ax.yaxis.set_major_locator(ticker.MultipleLocator(y_step))
+                ax.grid(which='major', axis='y', color='b')
+                for r, i in zip(u_re, u_im):
+                    ax.plot(r, i, color='b')
 
-    elif reim == IM:
-        if faxis == ORIG:
-            ax.xaxis.set_major_locator(ticker.MultipleLocator(x_step))
-            ax.grid(which='major', axis='x', color='tab:orange')
+        elif reim == IM:
+            if faxis == ORIG:
+                ax.xaxis.set_major_locator(ticker.MultipleLocator(x_step))
+                ax.grid(which='major', axis='x', color='tab:orange')
 
-        elif faxis == TRANSF:
-            for r, i in zip(v_re, v_im):
-                ax.plot(r, i, color='tab:orange')
+            elif faxis == TRANSF:
+                for r, i in zip(v_re, v_im):
+                    ax.plot(r, i, color='tab:orange')
 
-        else:
-            ax.xaxis.set_major_locator(ticker.MultipleLocator(x_step))
-            ax.grid(which='major', axis='x', color='tab:orange')
-            for r, i in zip(v_re, v_im):
-                ax.plot(r, i, color='tab:orange')
-
-    else:
-        if faxis == ORIG:
-            ax.xaxis.set_major_locator(ticker.MultipleLocator(x_step))
-            ax.grid(which='major', axis='x', color='tab:orange')
-            ax.yaxis.set_major_locator(ticker.MultipleLocator(y_step))
-            ax.grid(which='major', axis='y', color='b')
-
-        elif faxis == TRANSF:
-            for r, i in zip(u_re, u_im):
-                ax.plot(r, i, color='b')
-            for r, i in zip(v_re, v_im):
-                ax.plot(r, i, color='tab:orange')
+            else:
+                ax.xaxis.set_major_locator(ticker.MultipleLocator(x_step))
+                ax.grid(which='major', axis='x', color='tab:orange')
+                for r, i in zip(v_re, v_im):
+                    ax.plot(r, i, color='tab:orange')
 
         else:
-            ax.xaxis.set_major_locator(ticker.MultipleLocator(x_step))
-            ax.grid(which='major', axis='x', color='tab:orange')
-            ax.yaxis.set_major_locator(ticker.MultipleLocator(y_step))
-            ax.grid(which='major', axis='y', color='b')
-            for r, i in zip(u_re, u_im):
-                ax.plot(r, i, color='b')
-            for r, i in zip(v_re, v_im):
-                ax.plot(r, i, color='tab:orange')
+            if faxis == ORIG:
+                ax.xaxis.set_major_locator(ticker.MultipleLocator(x_step))
+                ax.grid(which='major', axis='x', color='tab:orange')
+                ax.yaxis.set_major_locator(ticker.MultipleLocator(y_step))
+                ax.grid(which='major', axis='y', color='b')
+
+            elif faxis == TRANSF:
+                for r, i in zip(u_re, u_im):
+                    ax.plot(r, i, color='b')
+                for r, i in zip(v_re, v_im):
+                    ax.plot(r, i, color='tab:orange')
+
+            else:
+                ax.xaxis.set_major_locator(ticker.MultipleLocator(x_step))
+                ax.grid(which='major', axis='x', color='tab:orange')
+                ax.yaxis.set_major_locator(ticker.MultipleLocator(y_step))
+                ax.grid(which='major', axis='y', color='b')
+                for r, i in zip(u_re, u_im):
+                    ax.plot(r, i, color='b')
+                for r, i in zip(v_re, v_im):
+                    ax.plot(r, i, color='tab:orange')
 
     if inits_point is not None:
-        for init in inits_point:
-            fun_init = [fun(item) for item in init]
-            fun_init_re = np.array([re(item) for item in fun_init])
-            fun_init_im = np.array([im(item) for item in fun_init])
-            ax.plot(fun_init_re, fun_init_im, color='g')
+        if faxis == TRANSF or faxis == BOTH:
+            for init in inits_point:
+                fun_init = [fun(item) for item in init]
+                fun_init_re = np.array([re(item) for item in fun_init])
+                fun_init_im = np.array([im(item) for item in fun_init])
+                ax.plot(fun_init_re, fun_init_im, color='g')
+        
+        if faxis == ORIG or faxis == BOTH:
+            for init in inits_point:
+                init_re = np.array([re(item) for item in init])
+                init_im = np.array([im(item) for item in init])
+                ax.plot(init_re, init_im, color='g')
 
     plt.xlim(xbound)
     plt.ylim(ybound)
