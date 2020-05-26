54078a8412e9c87a39dfc70d8e7880384c5f1185
Winkel <dmesser@yandex.ru>
2020-05-27 01:44:54 +0500

    Rework entire algorithm. Delete useless fuc and constants

--- plotcp/__init__.py
+++ plotcp/__init__.py
@@ -7,8 +7,6 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
 
-I: complex = 1j
-
 
 class Faxis(Flag):
     ORIG = auto()
@@ -22,12 +20,10 @@ class Reim(Flag):
     BOTH = RE | IM
 
 
-def re(z: complex) -> float:
-    return z.real
-
-
-def im(z: complex) -> float:
-    return z.imag
+class Inits(Flag):
+    ORIG = auto()
+    TRANSF = auto()
+    BOTH = ORIG | TRANSF
 
 
 def plotcp(
@@ -39,6 +35,7 @@ def plotcp(
         inits_point: Optional[Iterator] = None,
         faxis: Faxis = Faxis.BOTH,
         reim: Reim = Reim.BOTH,
+        inits: Inits = Inits.BOTH,
         inits_only: bool = False,
 ):
     if inits_only and inits_point is None:
@@ -46,100 +43,69 @@ def plotcp(
 
     fig, ax = plt.subplots()
 
+    plt.xlim(xbound)
+    plt.ylim(ybound)
+
+    if inits_point is not None:
+        if inits == Inits.TRANSF or inits == Inits.BOTH:
+            for init in inits_point:
+                fun_init = [fun(item) for item in init]
+                fun_init_re = np.real(fun_init)
+                fun_init_im = np.imag(fun_init)
+                ax.plot(fun_init_re, fun_init_im, color='g')
+
+        if inits == Inits.ORIG or inits == Inits.BOTH:
+            for init in inits_point:
+                init_re = np.real(init)
+                init_im = np.imag(init)
+                ax.plot(init_re, init_im, color='g')
+        
+        if inits_only:
+            return plt
+
     x_step = (xbound[1] - xbound[0]) / gridstep
     y_step = (ybound[1] - ybound[0]) / gridstep
+
+    if faxis == Faxis.ORIG or faxis == Faxis.BOTH:
+        if reim == Reim.RE or reim == Reim.BOTH:
+            ax.yaxis.set_major_locator(ticker.MultipleLocator(y_step))
+            ax.grid(which='major', axis='y', color='b')
+        
+        if reim == Reim.IM or reim == Reim.BOTH:
+            ax.xaxis.set_major_locator(ticker.MultipleLocator(x_step))
+            ax.grid(which='major', axis='x', color='tab:orange')
+        
+        if faxis != Faxis.BOTH:
+            return plt
 
     u_re = []
     u_im = []
     v_re = []
     v_im = []
 
-    if not inits_only:
-        if faxis == Faxis.TRANSF or faxis == Faxis.BOTH:
-            x = np.linspace(xbound[0], xbound[1], 2 * gridstep)
-            y = np.linspace(ybound[0], ybound[1], 2 * gridstep)
+    if faxis == Faxis.TRANSF or faxis == Faxis.BOTH:
+        x = np.linspace(xbound[0], xbound[1], 2 * gridstep)
+        y = np.linspace(ybound[0], ybound[1], 2 * gridstep)
 
-            if reim == Reim.RE or reim == Reim.BOTH:
-                x_parallels = [[item_x + item_y * I for item_x in np.linspace(xbound[0], xbound[1], nsteps)] for item_y
-                               in y]
-                u = [[fun(item) for item in xs] for xs in x_parallels]
-                u_re = [np.array([re(item) for item in us]) for us in u]
-                u_im = [np.array([im(item) for item in us]) for us in u]
+        if reim == Reim.RE or reim == Reim.BOTH:
+            x_parallels = [[item_x + item_y * 1j for item_x in np.linspace(xbound[0], xbound[1], nsteps)] for item_y
+                            in y]
+            u = [[fun(item) for item in xs] for xs in x_parallels]
+            u_re = [np.real(us) for us in u]
+            u_im = [np.imag(us) for us in u]
 
-            if reim == Reim.IM or reim == Reim.BOTH:
-                y_parallels = [[item_x + item_y * I for item_y in np.linspace(ybound[0], ybound[1], nsteps)] for item_x
-                               in x]
-                v = [[fun(item) for item in ys] for ys in y_parallels]
-                v_re = [np.array([re(item) for item in vs]) for vs in v]
-                v_im = [np.array([im(item) for item in vs]) for vs in v]
+            for r, i in zip(u_re, u_im):
+                ax.plot(r, i, color='b')
 
-        if reim == Reim.RE:
-            if faxis == Faxis.ORIG:
-                ax.yaxis.set_major_locator(ticker.MultipleLocator(y_step))
-                ax.grid(which='major', axis='y', color='b')
 
-            elif faxis == Faxis.TRANSF:
-                for r, i in zip(u_re, u_im):
-                    ax.plot(r, i, color='b')
+        if reim == Reim.IM or reim == Reim.BOTH:
+            y_parallels = [[item_x + item_y * 1j for item_y in np.linspace(ybound[0], ybound[1], nsteps)] for item_x
+                            in x]
+            v = [[fun(item) for item in ys] for ys in y_parallels]
+            v_re = [np.real(vs) for vs in v]
+            v_im = [np.imag(vs) for vs in v]
 
-            else:
-                ax.yaxis.set_major_locator(ticker.MultipleLocator(y_step))
-                ax.grid(which='major', axis='y', color='b')
-                for r, i in zip(u_re, u_im):
-                    ax.plot(r, i, color='b')
+            for r, i in zip(v_re, v_im):
+                ax.plot(r, i, color='tab:orange')
 
-        elif reim == Reim.IM:
-            if faxis == Faxis.ORIG:
-                ax.xaxis.set_major_locator(ticker.MultipleLocator(x_step))
-                ax.grid(which='major', axis='x', color='tab:orange')
-
-            elif faxis == Faxis.TRANSF:
-                for r, i in zip(v_re, v_im):
-                    ax.plot(r, i, color='tab:orange')
-
-            else:
-                ax.xaxis.set_major_locator(ticker.MultipleLocator(x_step))
-                ax.grid(which='major', axis='x', color='tab:orange')
-                for r, i in zip(v_re, v_im):
-                    ax.plot(r, i, color='tab:orange')
-
-        else:
-            if faxis == Faxis.ORIG:
-                ax.xaxis.set_major_locator(ticker.MultipleLocator(x_step))
-                ax.grid(which='major', axis='x', color='tab:orange')
-                ax.yaxis.set_major_locator(ticker.MultipleLocator(y_step))
-                ax.grid(which='major', axis='y', color='b')
-
-            elif faxis == Faxis.TRANSF:
-                for r, i in zip(u_re, u_im):
-                    ax.plot(r, i, color='b')
-                for r, i in zip(v_re, v_im):
-                    ax.plot(r, i, color='tab:orange')
-
-            else:
-                ax.xaxis.set_major_locator(ticker.MultipleLocator(x_step))
-                ax.grid(which='major', axis='x', color='tab:orange')
-                ax.yaxis.set_major_locator(ticker.MultipleLocator(y_step))
-                ax.grid(which='major', axis='y', color='b')
-                for r, i in zip(u_re, u_im):
-                    ax.plot(r, i, color='b')
-                for r, i in zip(v_re, v_im):
-                    ax.plot(r, i, color='tab:orange')
-
-    if inits_point is not None:
-        if faxis == Faxis.TRANSF or faxis == faxis.BOTH:
-            for init in inits_point:
-                fun_init = [fun(item) for item in init]
-                fun_init_re = np.array([re(item) for item in fun_init])
-                fun_init_im = np.array([im(item) for item in fun_init])
-                ax.plot(fun_init_re, fun_init_im, color='g')
-
-        if faxis == Faxis.ORIG or faxis == Faxis.BOTH:
-            for init in inits_point:
-                init_re = np.array([re(item) for item in init])
-                init_im = np.array([im(item) for item in init])
-                ax.plot(init_re, init_im, color='g')
-
-    plt.xlim(xbound)
-    plt.ylim(ybound)
     return plt
