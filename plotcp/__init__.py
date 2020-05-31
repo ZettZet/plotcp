aa45ca4d083a3d5f09844d50359546db6877da75
Winkel <dmesser@yandex.ru>
2020-05-31 15:41:36 +0500

    add polar parametrization

--- plotcp/__init__.py
+++ plotcp/__init__.py
@@ -32,12 +32,13 @@ def plotcp(
         x_bound: Tuple[int, int],
         y_bound: Tuple[int, int],
         n_steps: int = 100,
-        grid_step: int = 10,
+        grid_step: Tuple[int, int] = (10, 10),
         init_points: Optional[Iterator] = None,
         faxis: Faxis = Faxis.BOTH,
         reim: Reim = Reim.BOTH,
         inits: Inits = Inits.BOTH,
         inits_only: bool = False,
+        polar: bool = False,
 ) -> Axes:
     """
     :param fun: your predefined function f(z)
@@ -53,6 +54,7 @@ def plotcp(
     :param inits: what to display: 'origin', 'transform' or 'both' (named constants, correspondingly: Inits.ORIG,
     Inits.TRANSFORM, Inits.BOTH)
     :param inits_only: show initial points only? transformed or not
+    :param polar: use polar grid parametrization instead of cartesian
     :return: matplotlib Axes object
     """
     if inits_only and init_points is None:
@@ -80,43 +82,86 @@ def plotcp(
         if inits_only:
             return ax
 
-    x_step = (x_bound[1] - x_bound[0]) / grid_step
-    y_step = (y_bound[1] - y_bound[0]) / grid_step
+    x_step = 0
+    y_step = 0
+
+    if polar:
+        x_step = 2*np.pi/grid_step[0]
+        y_step = (max(x_bound[1], y_bound[1]) - min(x_bound[0], y_bound[0])) / grid_step[1]
+    else:
+        x_step = (x_bound[1] - x_bound[0]) / grid_step[0]
+        y_step = (y_bound[1] - y_bound[0]) / grid_step[1]
 
     if faxis & Faxis.ORIG:
-        if reim & Reim.RE:
-            ax.yaxis.set_major_locator(ticker.MultipleLocator(y_step))
-            ax.grid(which='major', axis='y', color='b')
+        if polar:
+            if reim & Reim.RE:
+                rays = [np.array([ro*(np.cos(x_step*k) + 1j*np.sin(x_step*k)) for ro in np.linspace(0, max(x_bound[1], y_bound[1])*1.5, n_steps)]) for k in range(grid_step[0])]
+                rays_re = [np.real(ray) for ray in rays]
+                rays_im = [np.imag(ray) for ray in rays]
+                for r, i in  zip(rays_re, rays_im):
+                    ax.plot(r, i, color='b')
+            
+            if reim & Reim.IM:
+                circles = [np.array([y_step*k*(np.cos(theta)+1j*np.sin(theta)) for theta in np.linspace(0, 2*np.pi, n_steps)]) for k in range(max(x_bound[0], y_bound[0], x_bound[1], y_bound[1])*2)]
+                circles_re = [np.real(circ) for circ in circles]
+                circles_im = [np.imag(circ) for circ in circles]
+                for r, i in  zip(circles_re, circles_im):
+                    ax.plot(r, i, color='tab:orange')
 
-        if reim & Reim.IM:
-            ax.xaxis.set_major_locator(ticker.MultipleLocator(x_step))
-            ax.grid(which='major', axis='x', color='tab:orange')
+        else:
+            if reim & Reim.RE:
+                ax.yaxis.set_major_locator(ticker.MultipleLocator(y_step))
+                ax.grid(which='major', axis='y', color='b')
+
+            if reim & Reim.IM:
+                ax.xaxis.set_major_locator(ticker.MultipleLocator(x_step))
+                ax.grid(which='major', axis='x', color='tab:orange')
 
         if faxis != Faxis.BOTH:
             return ax
 
     if faxis & Faxis.TRANSFORM:
-        x = np.linspace(x_bound[0], x_bound[1], 2 * grid_step)
-        y = np.linspace(y_bound[0], y_bound[1], 2 * grid_step)
+        if polar:
+            if reim & Reim.RE:
+                rays = [np.array([ro*(np.cos(y_step*k) + 1j*np.sin(y_step*k)) for ro in np.linspace(0, max(x_bound[1], y_bound[1])*1.5, n_steps)]) for k in range(grid_step[0])]
+                u = [[fun(item) for item in xs] for xs in rays]
+                u_re = [np.real(us) for us in u]
+                u_im = [np.imag(us) for us in u]
 
-        if reim & Reim.RE:
-            x_parallels = [[item_x + item_y * 1j for item_x in np.linspace(x_bound[0], x_bound[1], n_steps)] for item_y
-                           in y]
-            u = [[fun(item) for item in xs] for xs in x_parallels]
-            u_re = [np.real(us) for us in u]
-            u_im = [np.imag(us) for us in u]
+                for r, i in zip(u_re, u_im):
+                    ax.plot(r, i, color='b')
 
-            for r, i in zip(u_re, u_im):
-                ax.plot(r, i, color='b')
+            if reim & Reim.IM:
+                circles = [np.array([grid_step[1]*k*(np.cos(theta)+1j*np.sin(theta)) for theta in np.linspace(0, 2*np.pi, n_steps)]) for k in range(max(x_bound[1], y_bound[1]))]
+                v = [[fun(item) for item in vs] for vs in circles]
+                v_re = [np.real(vs) for vs in v]
+                v_im = [np.imag(vs) for vs in v]
 
-        if reim & Reim.IM:
-            y_parallels = [[item_x + item_y * 1j for item_y in np.linspace(y_bound[0], y_bound[1], n_steps)] for item_x
-                           in x]
-            v = [[fun(item) for item in ys] for ys in y_parallels]
-            v_re = [np.real(vs) for vs in v]
-            v_im = [np.imag(vs) for vs in v]
+                for r, i in zip(v_re, v_im):
+                    ax.plot(r, i, color='tab:orange')
 
-            for r, i in zip(v_re, v_im):
-                ax.plot(r, i, color='tab:orange')
+        else:
+            x = np.linspace(x_bound[0], x_bound[1], 2 * grid_step[0])
+            y = np.linspace(y_bound[0], y_bound[1], 2 * grid_step[1])
+
+            if reim & Reim.RE:
+                x_parallels = [[item_x + item_y * 1j for item_x in np.linspace(x_bound[0], x_bound[1], n_steps)] for item_y
+                            in y]
+                u = [[fun(item) for item in xs] for xs in x_parallels]
+                u_re = [np.real(us) for us in u]
+                u_im = [np.imag(us) for us in u]
+
+                for r, i in zip(u_re, u_im):
+                    ax.plot(r, i, color='b')
+
+            if reim & Reim.IM:
+                y_parallels = [[item_x + item_y * 1j for item_y in np.linspace(y_bound[0], y_bound[1], n_steps)] for item_x
+                            in x]
+                v = [[fun(item) for item in ys] for ys in y_parallels]
+                v_re = [np.real(vs) for vs in v]
+                v_im = [np.imag(vs) for vs in v]
+
+                for r, i in zip(v_re, v_im):
+                    ax.plot(r, i, color='tab:orange')
 
     return ax
