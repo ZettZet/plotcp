6f47db42037be259658b813df37c4f72f7ed2978
Fedor Kuznetsov <dmesser@yandex.ru>
2022-04-27 23:52:01 +0500

    global imports

--- plotcp/__init__.py
+++ plotcp/__init__.py
@@ -1,8 +1,9 @@
-__all__ = ['__version__', 'plotcp', 'plot_complex_points', 'Faxis', 'Reim']
-__version__ = '0.3.0'
-
-from plotcp.plotcp import plotcp
-
-from plotcp.plot_complex_points import plot_complex_points
+__all__ = ('__version__', 'plotcp_plt', 'plotcp_plotly', 'plot_complex_points_plt', 'plot_complex_points_plotly', 'Faxis', 'Reim')
+__version__ = '0.4.0'
 
 from plotcp.enums import Faxis, Reim
+
+from plotcp.plotcp_old import plotcp_plt
+from plotcp.plotcp_new import plotcp_plotly
+
+from plotcp.plot_complex_points import plot_complex_points_plt, plot_complex_points_plotly
